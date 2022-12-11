0898e44184b6d2bd552a2684ff758750f81becdc
Gefferson Casasola <gcasasolah@uni.pe>
2022-12-10 21:25:28 -0500

    config 2

--- djangoportfolio/settings.py
+++ djangoportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d00)7tk6z8-js-14ds4kk8*lx%_ht0mt8!y8h6wu29btd5*d3g'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
