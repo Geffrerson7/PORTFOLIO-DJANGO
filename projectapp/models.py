4714b795d7b0e40990c2eee5c427d125fec269d5
Gefferson Casasola <gcasasolah@uni.pe>
2023-01-02 15:48:02 -0500

    cambios en los templates

--- projectapp/models.py
+++ projectapp/models.py
@@ -5,11 +5,21 @@ from django.db.models.fields import CharField, URLField
 
 
 class Project(models.Model):
+    class tagsOption(models.TextChoices):
+        PYTHON = 'python', 
+        DJANGO = 'django',
+        FLASK = 'flask',         
+
+    tags = models.CharField(
+        max_length=10,
+        choices=tagsOption.choices,
+        default=tagsOption.PYTHON,
+    )
     title = CharField(max_length=200)
     description = models.TextField()
     url_image = URLField(blank=True)
     url_github = URLField(blank=True)
-    tags = CharField(max_length=100)
+    
 
     def __str__(self) -> str:
         return self.title
